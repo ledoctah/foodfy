4391d3ab155d96eb8aae1d8827fde9ee1e6b3239
ledoctah <matheusferr2015@gmail.com>
2020-02-23 19:38:34 -0300

    Added recipe page functionality

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,30 @@
 const cards = document.querySelectorAll('.food-card');
+const hideButtons = document.querySelectorAll('.hide');
 
 function addListeners() {
-    //add event to open modal
+    //add event to open recipe
     for(let card of cards) {
         const recipeId = card.getAttribute('id');
 
         card.addEventListener('click', function() {
             window.location.href = `/recipes/${recipeId}`;
         });
+    }
+
+    //add event to hide steps
+    for(let hideButton of hideButtons) {
+        hideButton.addEventListener('click', function() {
+            const container = document.querySelector(`#container-${hideButton.getAttribute('id')}`)
+
+            container.classList.toggle('hidden');
+            
+            if(container.classList.contains('hidden')) {
+                hideButton.innerText = 'Mostrar';
+            } else {
+                hideButton.innerText = 'Esconder';
+            }
+
+        })
     }
 }
 
