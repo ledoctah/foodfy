c216942e69f2fb5b1e841422811e170f6a897059
ledoctah <matheusferr2015@gmail.com>
2020-10-30 23:02:30 -0300

    Added on delete cascade constraint

--- database.sql
+++ database.sql
@@ -54,15 +54,20 @@ CREATE TABLE "recipe_files" (
   "file_id" int
 );
 
-ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+ON DELETE CASCADE;
 
-ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id")
+ON DELETE CASCADE;
 
-ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id");
+ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id")
+ON DELETE CASCADE;
 
-ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id")
+ON DELETE CASCADE;
 
-ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id");
+ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("id")
+ON DELETE CASCADE;
 
 CREATE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
